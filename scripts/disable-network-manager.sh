6429db77e536acdac503500a6a917939ea0770f0
Lauri Korts-Pärn <lauri@cyberdefense.jp>
2014-09-04 16:16:14 +0900

    relocate unrelated scripts

--- scripts/disable-network-manager.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-dbus-send --system --dest=org.freedesktop.NetworkManager --type=method_call --print-reply /org/freedesktop/NetworkManager org.freedesktop.NetworkManager.Enable boolean:false
