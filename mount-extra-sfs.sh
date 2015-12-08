d24cccd91a8c6f5b4a4865fe2bb637f9009bb980
Lauri Korts-Pärn <lauri@cyberdefense.jp>
2015-12-08 15:45:30 -0500

    mount-extra-sfs.sh: allow adding directories

--- mount-extra-sfs.sh
+++ mount-extra-sfs.sh
@@ -6,6 +6,7 @@ test -r "$1" || {
 }
 
 : ${rootfs:=/}
+: ${aufs_si=$(grep -F " / aufs " /proc/mounts | tail -1 | grep -o si=[^,[:space:]]* | cut -f2 -d=)}
 
 add_sfs() {
   local sfs="$1"
@@ -21,8 +22,21 @@ add_sfs() {
   mount -t aufs -o "remount,ins:1:$mnt=rr" none "$rootfs"
 }
 
+add_dir() {
+  local mnt="$1" br_name
+  local name="$(basename "$mnt")"
+  if br_name=$(grep -lwFr "$(readlink -f "$mnt")" "/sys/fs/aufs/si_${aufs_si}");then
+    echo -n "$name already mounted as ${br_name##*/} .. " >&2
+    return 1
+  else
+    mount -t aufs -o "remount,ins:1:$mnt=ro" "$name" "$rootfs"
+  fi
+}
+
 for sfs;do
   echo -n "Adding ${sfs##*/}.."
-  if add_sfs "$sfs";then echo ok
+  if test -d "$sfs";then
+    add_dir "$sfs" && echo ok || echo fail
+  elif add_sfs "$sfs";then echo ok
   else echo fail;fi
 done
