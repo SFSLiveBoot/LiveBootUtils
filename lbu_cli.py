4e2a30c0f6cb1da77170fad220821c709ccfd7b0
Lauri Korts-Pärn <lauri@cyberdefense.jp>
2017-11-21 11:20:39 -0500

    lbu_cli: move update-sfs to lbu_common

--- lbu_cli.py
+++ lbu_cli.py
@@ -3,8 +3,7 @@
 from logging import info, warn, error
 import logging
 import subprocess
-from lbu_common import SFSDirectory, get_root_sfs, CLIProgressReporter, stamp2txt, cli_func,\
-    SFSDirectoryAufs, BadArgumentsError
+from lbu_common import cli_func, BadArgumentsError
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -15,66 +14,6 @@ else:
     logging.addLevelName(logging.INFO, "{blue}{level}{reset}".format(level=logging.getLevelName(logging.INFO), **_log_colors))
     logging.addLevelName(logging.WARNING, "{yellow}{level}{reset}".format(level=logging.getLevelName(logging.WARNING), **_log_colors))
     logging.addLevelName(logging.ERROR, "{red}{level}{reset}".format(level=logging.getLevelName(logging.ERROR), **_log_colors))
-
-
-def update_sfs_parse_args(argv):
-    if argv[0] == '--no-act':
-        return [argv[1], True] + argv[2:], {}
-    else:
-        return [argv[0], False] + argv[1:], {}
-
-
-@cli_func(parse_argv=update_sfs_parse_args,
-          desc="Update (or list only) a SFS collection (by defaults components of '/')")
-def update_sfs(source_dir, no_act=False, *target_dirs):
-    """[--no-act] {--list | --auto-rebuild | <source_dir>} [<target_dirs>...]"""
-    if not source_dir[:2] == '--':
-        source_dir = SFSDirectory(source_dir)
-    target_dirs=map(SFSDirectory, target_dirs)
-    if not target_dirs: target_dirs=(SFSDirectoryAufs(), )
-    for target_dir in target_dirs:
-        last_dir=None
-        for sfs in target_dir.all_sfs:
-            if not sfs.parent_directory == last_dir:
-                last_dir=sfs.parent_directory
-                info("Processing directory: %s", last_dir)
-            try:
-                if "/" in sfs.symlink_target:
-                    info("Skipping non-local symlink: %s -> %s", sfs.basename, sfs.symlink_target)
-                    continue
-            except OSError: pass
-            if source_dir=='--list':
-                print sfs.path
-                continue
-            dst_sfs = sfs.curlink_sfs()
-            if source_dir=='--auto-rebuild':
-                if dst_sfs.git_source:
-                    info("Git repo for %s: %s", dst_sfs.basename, dst_sfs.git_source)
-                if dst_sfs.latest_stamp > dst_sfs.create_stamp:
-                    info("Rebuilding %s: %s > %s", dst_sfs.basename,
-                         stamp2txt(dst_sfs.latest_stamp), stamp2txt(dst_sfs.create_stamp))
-                    if not no_act:
-                        dst_sfs.rebuild_and_replace()
-                else:
-                    info("Keeping %s: latest %s %s current: %s", dst_sfs.basename,
-                         stamp2txt(dst_sfs.latest_stamp),
-                         "<" if dst_sfs.latest_stamp < dst_sfs.create_stamp else "=",
-                         stamp2txt(dst_sfs.create_stamp))
-                continue
-            src_sfs=source_dir.find_sfs(dst_sfs.basename)
-            if src_sfs is None:
-                warn("Not found from update source, skipping: %s", dst_sfs.basename)
-            elif src_sfs.create_stamp > dst_sfs.create_stamp:
-                info("Replacing %s from %s: %s > %s", dst_sfs.basename, src_sfs.parent_directory,
-                     stamp2txt(src_sfs.create_stamp), stamp2txt(dst_sfs.create_stamp))
-                if not no_act:
-                    dst_sfs.replace_with(src_sfs, progress_cb=CLIProgressReporter(src_sfs.file_size))
-            elif src_sfs.create_stamp == dst_sfs.create_stamp:
-                info("Keeping same %s: %s", dst_sfs.basename, stamp2txt(src_sfs.create_stamp))
-            else:
-                warn("Keeping newer %s: %s < %s",
-                     dst_sfs.basename, stamp2txt(src_sfs.create_stamp), stamp2txt(dst_sfs.create_stamp))
-
 
 if __name__ == '__main__':
     import sys, os
